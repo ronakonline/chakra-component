3db376d2a22436343b08237110a6134ab2afe9df
Ronak <32793372+ronakonline@users.noreply.github.com>
2021-10-20 15:00:01 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Container,
+  Box,
+  Image,
+  Text,
+  Heading,
+  Stack,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+} from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container minW="100vw" bg="#FEFFFE" minH="100vh" overflow="hidden" >
+      <Box
+        bg="white"
+        p={0}
+        mt={16}
+        boxShadow="xl"
+        borderRadius="lg"
+        overflow="hidden"
+        display="flex"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Stack  direction={["column-reverse", "row"]}> 
+          <Box p={16} flex="2">
+            <Heading>You Are Invited</Heading>
+            <Text mt={1} fontSize="22px">
+              Let the challenge begin
+            </Text>
+            <Text as="p" my={5}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Text>
+              <InputGroup>
+              <Input placeholder="Enter Email" _hover={{}} borderRadius="xl" borderColor="#ED399A" borderWidth="2px" _focus={{}}/>
+                <InputRightElement w="100px" >
+                 <Button borderBottomLeftRadius="2xl" bg="#ED399A" color="#FDFEF9" _hover={{}}>Join Me In</Button>
+                 </InputRightElement>
+              </InputGroup>
+          </Box>
+
+          <Box flex="2">
+            <Image
+              src="https://cdn.thetealmango.com/wp-content/uploads/2021/10/squiddd.jpg"
+              height="100%"
+              width="100%"
+              borderRightRadius="lg"
+            />
+          </Box>
+        </Stack>
+      </Box>
+    </Container>
   );
 }
 
